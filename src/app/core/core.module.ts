20aa819e12c217b2d5db5f51adfb4c4ee13994ce
christophe <christophegueroult@gmail.com>
2021-03-29 13:50:09 +0100

    install bootstrap and ngBootstrap with ng add

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [UiModule],
 })
 export class CoreModule {}
