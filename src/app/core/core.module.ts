3b4463874e0de9eba63f1e676911f401bc208118
christophe <christophegueroult@gmail.com>
2021-03-30 10:05:03 +0200

    templates module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -14,6 +15,7 @@ import { NavComponent } from './components/nav/nav.component';
     LoginModule,
     IconsModule,
     UiModule,
+    TemplatesModule,
     HeaderComponent,
     NavComponent,
     FooterComponent,
